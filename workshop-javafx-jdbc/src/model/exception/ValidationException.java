f1a30ca670f9ea29e2a510594c9aeb996a374597
Daniel Ahavat <daniel.ahavat@gmail.com>
2021-02-27 12:09:05 -0300

    Validation exception

---
+++ workshop-javafx-jdbc/src/model/exception/ValidationException.java
@@ -0,0 +1,28 @@
+package model.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, String> errors = new HashMap<>();
+	
+	//força a instanciacao da exceção com String
+	public ValidationException(String msg) {
+		super(msg);
+	}
+	
+	public  Map<String, String> getErrors() {
+		return errors;
+		
+	}
+	
+	public void addError(String fieldName, String errorMessage) {
+		errors.put(fieldName, errorMessage);
+	}
+	
+	
+	
+}
